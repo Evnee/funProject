082c65e97c4fba05d1285696fd20404cdec9a7da
Огнев Иван Владимирович <ognev.iv>
2024-01-02 01:12:08 +0300

    add theme module

--- theme/src/main/java/ru/evneeinc/theme/Typography.kt
+++ theme/src/main/java/ru/evneeinc/theme/Typography.kt
@@ -1,4 +1,4 @@
-package ru.evneeinc.funProject.ui.theme
+package ru.evneeinc.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
@@ -6,15 +6,14 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
+        letterSpacing = 0.5.sp,
+    ),
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
