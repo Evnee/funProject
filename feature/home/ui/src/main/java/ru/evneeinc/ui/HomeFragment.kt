50a5a647a98ebceaf5f88eeba1fa597d05156773
Огнев Иван Владимирович <ognev.iv>
2024-02-29 15:52:02 +0300

    add broken dialog

--- feature/home/ui/src/main/java/ru/evneeinc/ui/HomeFragment.kt
+++ feature/home/ui/src/main/java/ru/evneeinc/ui/HomeFragment.kt
@@ -2,7 +2,6 @@ package ru.evneeinc.ui
 
 import android.os.Bundle
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
@@ -11,8 +10,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.semantics.Role.Companion.Button
-import androidx.compose.ui.unit.sp
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.channels.Channel
 import ru.evneeinc.feature.ComposeFragment
@@ -36,12 +33,12 @@ class HomeFragment : ComposeFragment() {
                 .background(Color.White)
         ) {
             Button(
-                onClick = {router.showDialog(DialogType.TEST)},
+                onClick = { router.showDialog(DialogType.TEST) },
                 modifier = Modifier
                     .align(Alignment.Center)
-            ){
+            ) {
                 Text(text = "Open broken dialog")
-        }
+            }
         }
     }
 
